08502998d3915d978b8dfad0c145da954a685203
Swapnil Mishra <63197684+swap72@users.noreply.github.com>
2026-06-08 17:36:08 +0530

    Update main2.go
    Refactored main function to use goroutines and mutex for concurrent counter incrementing.

--- Going in Go/main2.go
+++ Going in Go/main2.go
@@ -1,14 +1,69 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+// func main() {
+// 	var limit int
+// 	fmt.Print("Enter the Slice Limit: ")
+// 	fmt.Scan(&limit)
+// 	nums := []int{}
+// 	for i := 1; i <= limit; i++ {
+// 		nums = append(nums, i)
+// 	}
+// 	fmt.Println(nums)
+// }
+
+// func main() {
+// 	for {
+// 		go greet()
+// 	}
+// }
+// // spawn infinite GO routines
+// func greet() {
+// 	fmt.Println("Hi, how are you?")
+// }
 
 func main() {
-	var limit int
-	fmt.Println("Enter the Slice Limit: ")
-	fmt.Scan(&limit)
-	nums := []int{}
-	for i := 1; i <= limit; i++ {
-		nums = append(nums, i)
-	}
-	fmt.Println(nums)
+	var counter int
+	var mu sync.Mutex
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		for i := 1; i <= 3000; i++ {
+			mu.Lock()
+			counter++
+			mu.Unlock()
+		}
+		defer wg.Done()
+	}()
+	wg.Add(1)
+	go func() {
+		for i := 1; i <= 3000; i++ {
+			mu.Lock()
+			counter++
+			mu.Unlock()
+		}
+		defer wg.Done()
+	}()
+	wg.Add(1)
+	go func() {
+		for i := 1; i <= 3000; i++ {
+			mu.Lock()
+			counter++
+			mu.Unlock()
+		}
+		defer wg.Done()
+	}()
+	wg.Wait()
+	fmt.Println(counter)
 }
+
+/*
+AI recommendations :
+defer mu.Unlock() more reliable
+use of atomic counter preffered
+*/
