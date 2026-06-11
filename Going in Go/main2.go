71edba3cd0d0523de4c0afdfd385c8728d693196
Swapnil Mishra <63197684+swap72@users.noreply.github.com>
2026-06-11 17:24:37 +0530

    Update main2.go

--- Going in Go/main2.go
+++ Going in Go/main2.go
@@ -26,44 +26,68 @@ import (
 // 	fmt.Println("Hi, how are you?")
 // }
 
-func main() {
-	var counter int
-	var mu sync.Mutex
-	var wg sync.WaitGroup
+// func main() {
+// 	var counter int
+// 	var mu sync.Mutex
+// 	var wg sync.WaitGroup
 
-	wg.Add(1)
-	go func() {
-		for i := 1; i <= 3000; i++ {
-			mu.Lock()
-			counter++
-			mu.Unlock()
-		}
-		defer wg.Done()
-	}()
-	wg.Add(1)
-	go func() {
-		for i := 1; i <= 3000; i++ {
-			mu.Lock()
-			counter++
-			mu.Unlock()
-		}
-		defer wg.Done()
-	}()
-	wg.Add(1)
-	go func() {
-		for i := 1; i <= 3000; i++ {
-			mu.Lock()
-			counter++
-			mu.Unlock()
-		}
-		defer wg.Done()
-	}()
-	wg.Wait()
-	fmt.Println(counter)
-}
+// 	wg.Add(1)
+// 	go func() {
+// 		for i := 1; i <= 3000; i++ {
+// 			mu.Lock()
+// 			counter++
+// 			mu.Unlock()
+// 		}
+// 		defer wg.Done()
+// 	}()
+// 	wg.Add(1)
+// 	go func() {
+// 		for i := 1; i <= 3000; i++ {
+// 			mu.Lock()
+// 			counter++
+// 			mu.Unlock()
+// 		}
+// 		defer wg.Done()
+// 	}()
+// 	wg.Add(1)
+// 	go func() {
+// 		for i := 1; i <= 3000; i++ {
+// 			mu.Lock()
+// 			counter++
+// 			mu.Unlock()
+// 		}
+// 		defer wg.Done()
+// 	}()
+// 	wg.Wait()
+// 	fmt.Println(counter)
+// }
 
 /*
 AI recommendations :
 defer mu.Unlock() more reliable
 use of atomic counter preffered
 */
+
+// func main() {
+// 	content := []byte("This is the content to be written in a file")
+// 	err := os.WriteFile("test.txt", content, 0644)
+// 	if err != nil {
+// 		fmt.Print("Could not write to the file")
+// 		return
+// 	}
+// 	fmt.Print("File write successfull!")
+// }
+
+// func main() {
+// 	ar := [9000]int{}
+// 	filler(&ar)
+// 	fmt.Println(ar)
+// }
+
+// func filler(ar *[9000]int) {
+// 	for i := 0; i <= 3000; i++ {
+// 		if ar[len(ar)-1] == 0 {
+// 			ar[i] = i
+// 		}
+// 	}
+// }
